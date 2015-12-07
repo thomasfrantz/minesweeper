62be737beacfa6d9b332ce52d1a191a619b9fd52
thomasfrantz <thom.frantz@gmail.com>
2015-12-07 17:59:33 +0100

    linting lint.js

--- lint/lint.js
+++ lint/lint.js
@@ -1,14 +1,40 @@
 var glob = require("glob");
 var fs = require("fs");
-var eslint = require("eslint").CLIEngine;
-var linter = new eslint({configFile:"lint/.eslintrc", fix:true});
+
+// Configuring the linter tool
+var Eslint = require("eslint").CLIEngine;
+var linter = new Eslint({
+    "configFile": "lint/.eslintrc",
+    "fix":        false
+});
+
+// Colouring the command line
+var cliColor = require("cli-color");
+var errorC = cliColor.bold.red;
+var warningC = cliColor.bold.yellow;
+var linkC = cliColor.bold.cyan;
 
 // We parse every js files
-glob("src/client/*.js", function(err, files){
+glob("src/client/*.js", function outputESlint(err, files){
+    var i;
+    var reports;
+
     if(err){
         return console.error(err);
     }
-    var report = linter.executeOnFiles(files);
-    console.log(report.results[0].messages);
-    fs.writeFileSync(files[0], report.results[0].output);
+
+    files.push("lint/lint.js");
+    reports = linter.executeOnFiles(files);
+
+    for(i = 0; i < reports.results.length; i++){
+        console.log(
+            linkC(reports.results[ i ].filePath) + " contains :\n" +
+            errorC(reports.results[ i ].errorCount + " errors") + "\n" +
+            warningC(reports.results[ i ].warningCount + " warnings") + "\n\n"
+        );
+        if(reports.results[ 1 ].output){
+            fs.writeFileSync(files[ 1 ], reports.results[ 1 ].output);
+        }
+    }
+    //console.log(reports.results[ 1 ]);
 });
