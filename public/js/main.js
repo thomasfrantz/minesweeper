346c9b315f5c6345ce56f5065586e21c7f060c9a
thomasfrantz <thom.frantz@gmail.com>
2016-06-13 18:02:39 +0200

    disgusting updates not following lint rules :'( (but minesweeper is okay :p )

--- public/js/main.js
+++ public/js/main.js
@@ -1,105 +1,345 @@
-var squareSize = 50;
-
-var conf = {
-  horiSize: 10,
-  vertSize: 6,
-  mineNumber: 20
-};
+/**
+ *
+ *  PUBLIC METHODS : 
+ *  CheckMines() -Returns-> array of coordinates+value [{x:5,y:0,value:mine}] or [{x:4,y:0,value:1}] or [{x:2,y:0,value:blank}{x:2,y:1,value:blank}]
+ *
+ * make a saved map of already played 
+ * 
+ */
 
 function mineSweeper (div, conf){
-
-
-  function isMine(mine, coord){
-    return (mine.x === coord.x && mine.y === coord.y);
-  };
-
-  var isThere = memoize(function(mineField, coord){
-    var already = false;
-    mineField.forEach(function(mine){
-      if(isMine(mine, coord)) {
-        already = true;
-      }
-    });
-    return already;
-  });
-
-  function setMines(conf){
-    var mineField = [];
-    var i = 0;
-    while(i < conf.mineNumber){
-      var coord = {
-        "x" : Math.floor(Math.random()*conf.horiSize),
-        "y" : Math.floor(Math.random()*conf.vertSize)
-      };
-      if(!isThere(mineField, coord)){
-        mineField.push(coord);
-        i++; 
-      }
-    }
-    return mineField;
-  }
 
   var state = {
     "board" : div,
     "conf" : conf,
-    "mineField" : setMines(conf)
+    "mineField" : [],
+    "status" : "null"
   };
+
+  function getState(){
+    return state;
+  }
+
+  function getBoard(){
+    return state.board;
+  }
+
+  function getConf(){
+    return state.conf;
+  }
+
+  function getMineField(){
+    return state.mineField;
+  }
+
+  function setMineField(newMineField){
+    state.mineField = newMineField;
+  }
+
+  function getStatus(){
+    return state.status;
+  }
+
+  function setStatus(newState){
+    if(typeof newState === "string"){
+      return state.status = newState;
+    }else{
+      throw "Wrong typeof for newState";
+    }
+  }
+
+  function isInside(coord){
+    var conf = getConf();
+
+    return (coord.y<conf.vertSize && coord.y>=0 && coord.x<conf.horiSize && coord.x>=0);
+  }
+
+  function isMine(coord){
+    var mineField = getMineField();
+    return mineField[coord.y][coord.x].value === "m";
+  }
+
+
+  function initiateBoard(){
+    var conf = getConf();
+    var mineField = getMineField();
+
+    if(conf.mineNumber >= conf.horiSize*conf.vertSize){
+      throw "The number of mines is too high compared to the number of squares";
+    }
+
+    for(var y = 0; y < conf.vertSize; y++){
+      var hor = [];
+      for(var x = 0; x < conf.horiSize;x++){
+        var square = {
+          "x":x,
+          "y":y,
+          "value":0,
+          "show":false,
+          "clicked":false,
+          "flag":false
+        };
+        hor.push(square);
+      }
+      mineField.push(hor);
+    }
+    //setMineField(mineField);
+    setStatus("Board initiated");
+  }
+
+  function incrementNeighbours(mine){
+    var mineField = getMineField();
+    for (var i = -1; i <= 1; i++){
+      for (var j = -1; j <= 1; j++){
+        neighbourX = mine.x+j;
+        neighbourY = mine.y+i;
+        if(isInside({"y" : neighbourY, "x" : neighbourX}) && !isMine(mineField[neighbourY][neighbourX])){
+          mineField[neighbourY][neighbourX].value += 1;
+        }
+      }
+    }
+    //setMineField(mineField);
+  }
+
+  function setValues(){
+    var conf = getConf();
+    var mineField = getMineField();
+
+    if(getStatus() !== "Board initiated" ){
+      throw "Wrong state";
+    }
+
+    var i = 0;
+    while(i < conf.mineNumber){
+      var x = Math.floor(Math.random()*conf.horiSize);
+      var y = Math.floor(Math.random()*conf.vertSize);
+      if(!isMine(mineField[y][x])){
+        mineField[y][x].value = "m";
+        incrementNeighbours({"x" : x, "y" : y});
+        i++;
+      }
+    }
+    //setMineField(mineField);
+    setStatus("Values set");
+  }
+
+  function renderBoard(){
+    var board = getBoard();
+    var conf = getConf();
+
+    board.innerHTML = "";
+    board.style.width = (conf.squareSize+2)*conf.horiSize+"px";
+    for(var y = 0; y<conf.vertSize; y++){
+      for(var x = 0; x<conf.horiSize;x++){
+        var square = document.createElement("div");
+        square.className = "square "+x+";"+y;
+        square.style.width = conf.squareSize+"px";
+        square.style.height = conf.squareSize+"px";
+        square.style.lineHeight = conf.squareSize+"px";
+        square.style.textAlign = "center";
+        board.appendChild(square);
+      }
+    }
+    setStatus("Board rendered");
+  }
+
+  function checkSquare(coord){
+    var mineField = getMineField();
+    var square = mineField[coord.y][coord.x];
+    var state = getState();
+
+    if(square.show === false && square.flag === false){
+      square.show = true;
+      square.clicked = true;
+
+      if(square.value === 0){
+        for (var i = -1; i <= 1; i++){
+          for (var j = -1; j <= 1; j++){
+            neighbourX = square.x+j;
+            neighbourY = square.y+i;
+            neighbour = {"y" : neighbourY, "x" : neighbourX};
+            if(isInside(neighbour) && mineField[neighbourY][neighbourX].show !== true){
+              state = checkSquare({"y":neighbourY, "x":neighbourX});
+            }
+          }
+        }
+      }else if(square.value === "m"){
+        alert("Boom");
+        setStatus("Game Over");
+      }
+    }
+    
+    return state;
+  }
+
+  function leftClick(e){
+    var squareDiv = e.target;
+
+    var coordRegExp = /(\d*;\d*)/;
+    var coord = coordRegExp.exec(squareDiv.className)[0].split(";");
+
+    var newState = checkSquare({"x":coord[0],"y":coord[1]});
+
+    renderState(newState);
+  }
+
+  function addLeftClick(coord){
+    var board = getBoard();
+    var squareIndex = coord.x+coord.y*conf.vertSize;
+    var square = board.childNodes[squareIndex];
+
+    square.addEventListener("click", leftClick);
+  }
+
+  function flagSquare(coord){
+    var mineField = getMineField();
+    var square = mineField[coord.y][coord.x];
+    var state = getState();
+
+    if(square.show === false){
+      square.flag = !square.flag;
+    }
+    
+    return state;
+  }
+
+  function rightClick(e){
+    var squareDiv = e.target;
+
+    var coordRegExp = /(\d*;\d*)/;
+    var coord = coordRegExp.exec(squareDiv.className)[0].split(";");
+
+    var newState = flagSquare({"x":coord[0],"y":coord[1]});
+
+    renderState(newState);
+    return false;
+  }
+
+  function addRightClick(coord){
+    var board = getBoard();
+    var squareIndex = coord.x+coord.y*conf.vertSize;
+    var square = board.childNodes[squareIndex];
+
+    square.addEventListener("contextmenu", rightClick, false);
+  }
+
+  function addEvents(){
+
+    if(getStatus() !== "Board rendered" ){
+      throw "Wrong state";
+    }
+
+    for(var y = 0; y<conf.vertSize; y++){
+      for(var x = 0; x<conf.horiSize;x++){
+        addLeftClick({"x":x, "y":y});
+        addRightClick({"x":x, "y":y});
+      }
+    }
+    setStatus("Ready to click");
+
+  }
+
+  function start(){
+
+    initiateBoard();
+    setValues();
+    renderBoard();
+    addEvents();
+
+    var status = getStatus();
+    if(status !== "Ready to click" ){
+      throw "Problem after : "+status;
+    }
+  }
+
+  function renderSquare(coord, value, isClicked){
+    var conf = getConf();
+    var board = getBoard();
+      
+    var valueRegExp = / value([0-8]|m)/;
+    var squareIndex = coord.x+coord.y*conf.vertSize;
+    var square = board.childNodes[squareIndex];
+
+    if(!valueRegExp.test(square.className)){
+      if(value === "flag"){
+        square.className += " flag";
+      }else{
+        square.className += " show value"+value;
+        square.innerHTML= value;
+        if(!isClicked){
+          square.className += " notClicked";
+        }
+      }
+    }
+  }
+
+  function renderState(newState){
+    var board = newState.board;
+    var mineField = newState.mineField;
+    var status = newState.status;
+
+    //if(status !== "Game ongoing"){
+    if(status === "Game Over"){
+      for(var y = 0; y < conf.vertSize; y++){
+        for(var x = 0; x < conf.horiSize;x++){
+          renderSquare({"x":x, "y":y}, mineField[y][x].value, mineField[y][x].clicked);  
+        }
+      }
+      setStatus("Fail");
+    }else if(status !== "Ready to click"){
+      throw "Problem after : "+status+"\n State can't be rendered off a game";
+    }
+
+    for(var y = 0; y < conf.vertSize; y++){
+      for(var x = 0; x < conf.horiSize;x++){
+        if(mineField[y][x].show){
+          renderSquare({"x":x, "y":y}, mineField[y][x].value, true);
+        }else if(mineField[y][x].flag){
+          renderSquare({"x":x, "y":y}, "flag", false);
+        }
+      }
+    }
+  }
 
   return {
 
-    "reset" : function(){
-      state.board.innerHTML = "";
-    },
-
-    "render" : function(){
-      this.reset();
-      state.board.style.width = (squareSize+2)*state.conf.horiSize+"px";
-      for(var y = 0; y<state.conf.vertSize; y++){
-        for(var x = 0; x<state.conf.horiSize;x++){
-          var square = document.createElement("div");
-          square.className = x+";"+y;
-          square.style.width = squareSize+"px";
-          square.style.height = squareSize+"px";
-          square.addEventListener("click",this.checkMines);
-          state.board.appendChild(square);
+    "start" : start,
+    "renderState" : renderState,
+    "show" : function(){
+      var board = "";
+      for(var y = 0; y < conf.vertSize; y++){
+        for(var x = 0; x < conf.horiSize;x++){
+          board+= "[x:"+x+"; y:"+y+"; value:"+state.mineField[y][x].value+"]";
         }
+        board += "\n";
       }
-    },
-
-    "showMines" : function(){
-      console.log(state.mineField);
-      state.mineField.forEach(function(mine){
-        var square = document.getElementsByClassName(mine.x+";"+mine.y)[0];
-        square.style.backgroundColor = "red";
-      });
-    },
-
-    "checkMines" : function(clickedSquare){
-      var coord = clickedSquare.target.className.split(";");
-      
-      var clickedCoord = {
-        "x" : parseInt(coord[0]),
-        "y" : parseInt(coord[1])
-      }
-      if( isThere(state.mineField, clickedCoord)){
-        console.log("Perdu :p");
-      }else{
-
-      };
+      console.log(board);
     }
 
   };
 };
 
+
+
+var squareSize = 50;
+
+var conf = {
+  "horiSize": 10,
+  "vertSize": 10,
+  "mineNumber": 12,
+  "squareSize": 70
+};
+
 var game = mineSweeper(document.getElementsByClassName("grid")[0], conf);
-game.render();
-game.showMines();
+game.start();
+//game.show();
 
 /**
  * memoize function, by @philogb, Addy Osmani, Mathias Bynens and Dmitry Baranovsky. Seen on https://addyosmani.com/blog/faster-javascript-memoization/
  * @param  {Function} fn : function to be memoized
  * @return {Function}      : memoized function
  */
+
 function memoize( fn ) {
     return function () {
         var args = Array.prototype.slice.call(arguments),
--- test/tester.js
+++ test/tester.js
@@ -1,33 +1,73 @@
+var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+var assert = chai.assert;
+
+var a = 4;
+
+var t = assert.eventually.equal(Promise.resolve(2 + 2), 5, "This had better be true, eventually");
+t.then(function(val){console.log(t)});
+console.log(assert.isFulfilled(t, "yolo").then(function(val){console.log(val)}));
 function isPromise(func){
     return func !== null 
         && ( typeof func === "object" || typeof func === "function" ) 
         && typeof func.then === "function";
 }
 
-var tester = function(name){
-    var name = name || "tester" ;
-    var tests = [];
+function object2array (obj){
+    var arr = [];
+    for(var prop in obj){
+        if(obj.hasOwnProperty(prop)){
+            arr.push(obj[prop]);
+        }
+    }
+    return arr;
+}
+
+function tester (name){
+
+    var name = name || "function from file.js" ;
+    var tests = {};
+
+/*** @TODO
+ATTENTION FUTUR MOI : TESTS DOIT ETRE UN OBJET, POUR POUVOIR LEUR DONNER DES NOMS. 
+//IL FAUT FAIRE UN CONVERTISSEUR OBJET => ARRAY 
+IL FAUT FAIRE LA METHODE REMOVE
+METTRE ISPROMISE ET TOARRAY DANS UN FICHIER UTIL
+CHAI_AS_PROMISE
+**/
 
     return {
-        "register" : function(test){
-            if (isPromise(test)){
-                tests.push(test);
+        "register" : function(name, test){
+            var isP = isPromise(test);
+
+
+            if ( isP && name){
+                tests[name]= test;
+            }else if(!isP){
+                console.log(name + " n'est pas un objet Promise");
             }else{
-                console.log(test + " n'est pas un objet Promise");
+                console.log("Le nom doit être un String non vide");
             }
         },
-        "run" :      function(){
+        "remove" : function(name){
+            delete tests[name];
+        },
+        "run" : function(names){
+
+            /*
             var resP = new Promise(function(resolve, reject){
                 tests.forEach(function(test, i){
                     results[i] = test.then(function(val){return val});
                 });
                 resolve();
             });
-            resP.then(function(){console.log(tests, results)});
+            resP.then(function(){console.log(tests, results)});*/
+            console.log(object2array(tests));
         }
     }
 };
-
+/*
 var tests1 = tester();
 var test1 = "pasPromise";
 var test2 = new Promise( function(resolve, reject){
@@ -37,7 +77,7 @@ var test3 = new Promise( function(resolve, reject){
     resolve("Promise2");
 });
 
-tests1.register(test1);
-tests1.register(test2);
-tests1.register(test3);
-tests1.run();
+tests1.register("test1", test1);
+tests1.register("test2", test2);
+tests1.register("test3", test3);
+tests1.run();*/
